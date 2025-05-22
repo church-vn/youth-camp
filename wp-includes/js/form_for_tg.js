b6ab11ab5b531e3ea18d59f19aefa51445ab4701
PandaDevelopmentCompany <pandadesignstudio@yandex.ru>
2025-05-22 07:44:10 +0300

    download_22_05_2025_1

--- wp-includes/js/form_for_tg.js
+++ wp-includes/js/form_for_tg.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("registrationForm");
 
@@ -10,28 +9,44 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   function sendTelegramMessage() {
-    const token = "7610624510:AAGt4QFSKDfnjf9k9YhCb2OsjiuTRuQ9-JA"; // Замените сюда
-    const chatId = "-4947131625";  // Замените сюда
+    const token = "7610624510:AAGt4QFSKDfnjf9k9YhCb2OsjiuTRuQ9-JA";
+    const chatId = "-1002575821012";
 
     // Собираем значения
-    const name = document.getElementById("sendName_camp").value;
     const secondName = document.getElementById("sendSecondName_camp").value;
+    const name = document.getElementById("sendName_camp").value;
+    const middleName = document.getElementById("sendMiddleName_camp").value;
     const age = document.getElementById("sendAge_camp").value;
+    const birthDate = document.getElementById("birthDate_camp").value;
     const city = document.getElementById("sendCity_camp").value;
     const church = document.getElementById("sendChurch_camp").value;
+    const pastorName = document.getElementById("sendPastorName_camp").value;
+    const contraindications = document.getElementById("sendContraindications_camp").value;
+    const medication = document.getElementById("sendMedication_camp").value;
     const parent1 = document.getElementById("sendParent1Name_camp").value;
     const phone1 = document.getElementById("sendParent1Phone_camp").value;
+    const parent2 = document.getElementById("sendParent2Name_camp").value;
+    const phone2 = document.getElementById("sendParent2Phone_camp").value;
 
     const message = `
 🆕 Новая заявка на участие в лагере:
 
-👦 Имя: ${name}
 👪 Фамилия: ${secondName}
+👦 Имя: ${name}
+👤 Отчество: ${middleName}
 📅 Возраст: ${age}
+🎂 Дата рождения: ${birthDate}
 🏙️ Город: ${city}
 ⛪ Церковь: ${church}
+👨‍✝️ ФИО пастора: ${pastorName}
+⚠️ Противопоказания: ${contraindications}
+💊 Прием лекарств: ${medication}
+
 👨‍👩‍👧 Родитель 1: ${parent1}
-📞 Телефон родителя: <a href="tel:${phone1}">${phone1}</a>
+📞 Телефон 1: <a href="tel:${phone1}">${phone1}</a>
+
+👨‍👩‍👧 Родитель 2: ${parent2}
+📞 Телефон 2: <a href="tel:${phone2}">${phone2}</a>
     `;
 
     // Отправка в Telegram
